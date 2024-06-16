2c05b2cf772ceb6baa37dfaac9bd676e301b8ee9
focus.btw <kozyrchikov.nikita@gmail.com>
2024-06-17 02:34:49 +0500

    remove sait

--- beznadega/main/urls.py
+++ beznadega/main/urls.py
@@ -3,5 +3,24 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('cours', views.about, name='about')
+    path('cours', views.about, name='about'),
+    path('openedu', views.pyti, name='pyti'),
+    path('openedu.urfu', views.pyti1, name='pyti1'),
+    path('learnonline.hse.ru', views.pyti2, name='pyti2'),
+    path('lxp.utmn.ru', views.pyti3, name='pyti3'),
+    path('ULEARN.ME', views.pyti4, name='pyti4'),
+    path('ELEARN.URFU.RU', views.pyti5, name='pyti5'),
+    path('FAQ', views.FAQ, name='FAQ'),
+    path('course', views.course, name='course'),
+    path('URFU', views.course1, name='course1'),
+    path('ITMO', views.course2, name='course2'),
+    path('MGY', views.course3, name='course3'),
+    path('MISIS', views.course4, name='course4'),
+    path('MIFI', views.course5, name='course5'),
+    path('MFTI', views.course6, name='course6'),
+    path('NIY', views.course7, name='course7'),
+    path('POLITEH', views.course8, name='course8'),
+    path('SPB', views.course9, name='course9'),
+    path('TGY', views.course10, name='course10'),
+    path('TUMGY', views.course11, name='course11'),
 ]
--- beznadega/main/views.py
+++ beznadega/main/views.py
@@ -7,3 +7,96 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def pyti(request):
+    return render(request, 'main/pyti.html')
+
+
+def pyti1(request):
+    return render(request, 'main/pyti1.html')
+
+
+def pyti2(request):
+    return render(request, 'main/pyti2.html')
+
+
+def pyti3(request):
+    return render(request, 'main/pyti3.html')
+
+
+def pyti4(request):
+    return render(request, 'main/pyti4.html')
+
+
+def pyti5(request):
+    return render(request, 'main/pyti5.html')
+
+
+def FAQ(request):
+    return render(request, 'main/FAQ.html')
+
+
+def course(request):
+    return render(request, 'main/course.html')
+
+
+def course1(request):
+    return render(request, 'main/course1.html')
+
+
+def course2(request):
+    return render(request, 'main/course2.html')
+
+
+def course3(request):
+    return render(request, 'main/course3.html')
+
+
+def course4(request):
+    return render(request, 'main/course4.html')
+
+
+def course5(request):
+    return render(request, 'main/course5.html')
+
+
+def course6(request):
+    return render(request, 'main/course6.html')
+
+
+def course7(request):
+    return render(request, 'main/course7.html')
+
+
+def course8(request):
+    return render(request, 'main/course8.html')
+
+
+def course9(request):
+    return render(request, 'main/course9.html')
+
+
+def course10(request):
+    return render(request, 'main/course10.html')
+
+
+def course11(request):
+    return render(request, 'main/course11.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
